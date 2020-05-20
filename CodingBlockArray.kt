654da86a1e695cb1b6fe0967f114cb3160825528
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-05-20 11:18:16 +0530

    Create CodingBlockArray.kt

---
+++ CodingBlockArray.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    var numbers= arrayOf(1,2,3,4,5)
+    var mix= arrayOf(1,2,3,4,"Lavish","hi fi")
+    var Stringonly= arrayOf("Lavish","is ","a","good","boy")
+    for(i in numbers)
+    {
+        print("$i ")
+    }
+    println()
+    for(i in Stringonly)
+    {
+        print("$i ")
+    }
+    println()
+
+}
