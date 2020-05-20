3600385ed7d0488f75cc4161304439bb9550e8cf
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-05-20 14:55:18 +0530

    Update CodingBlockArray.kt

--- CodingBlockArray.kt
+++ CodingBlockArray.kt
@@ -13,5 +13,28 @@ fun main()
         print("$i ")
     }
     println()
+    
+    //see below you will surely learn a lot
+    val experiment=Array(6,{i->i*2})
+    println(experiment[2])
+    println(experiment[4])
+    val arr= intArrayOf()
+    for(i in arr)
+    {
+        print("$i ")
+    }
+    println()
+    val nullarray= arrayOfNulls<String>(10)
+    for(i in nullarray)
+    {
+        print("$i  ")
+    }
+    println()
+    arr.set(2,21)
+    print("new array is ")
+    for(i in arr)
+    {
+        print("$i ")
+    }
 
 }
