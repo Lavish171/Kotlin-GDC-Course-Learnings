4b95532267ffcfef75c6d4e8f38a0d45ce9bc8ca
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-05-20 11:19:09 +0530

    Update CodingBlockArray.kt

--- CodingBlockArray.kt
+++ CodingBlockArray.kt
@@ -1,8 +1,8 @@
 fun main()
 {
     var numbers= arrayOf(1,2,3,4,5)
-    var mix= arrayOf(1,2,3,4,"Lavish","hi fi")
-    var Stringonly= arrayOf("Lavish","is ","a","good","boy")
+    var mix= arrayOf(1,2,3,4,"Lavish","hi fi")//can contains both
+    var Stringonly= arrayOf("Lavish","is ","a","good","boy") //strictly contains only strings 
     for(i in numbers)
     {
         print("$i ")
