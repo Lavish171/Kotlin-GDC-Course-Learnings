2850dd400a0c4e246362e6ab699a9e17688e0a82
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-05-21 09:25:01 +0530

    Update CodingBlocksFunction.kt

--- CodingBlocksFunction.kt
+++ CodingBlocksFunction.kt
@@ -4,10 +4,17 @@ fun square1(x:Int):Int
 }
 fun square2(x:Int):Int=x*x
 fun square3(x:Int)=x*x
+
+fun showmeuser(name:String,age:Int=18,con:Boolean=false)
+{
+    println("Name is $name and the age is $age and the con is $con")
+}
+
 fun main()
 {
     var one:Int=square1(4)
     var two:Int=square2(5)
     var three:Int=square3(6)
     print("First function $one \n Second Function $two \n Third Function $three")
+    showmeuser("lavish",20,true)
 }
