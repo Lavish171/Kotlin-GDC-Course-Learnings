176f2a1edb676ffcc763c1b83d0d7039ee022fb1
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-05-26 19:05:07 +0530

    Create **Any_Class_Kotlin.kt

---
+++ **Any_Class_Kotlin.kt
@@ -0,0 +1,5 @@
+the any class is the super class of the all the classes in the kotlin.
+ Every Kotlin class has Any as a superclass.
+ val and const keyowrd
+ Companion in kotlin
+ No,type declaration becomes mandatory if you’re not initializing the variable at the time of declaration
